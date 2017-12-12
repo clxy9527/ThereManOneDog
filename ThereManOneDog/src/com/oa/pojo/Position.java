d72cf3bd0e4d0bda2a7a98397b03f9201fe53529
6170 <6170@C206170>
2017-12-12 11:45:55 +0800

    V0.19 提交部分pojo

---
+++ ThereManOneDog/src/com/oa/pojo/Position.java
@@ -0,0 +1,25 @@
+package com.oa.pojo;
+
+public class Position {
+	private int pId;
+	private String pName;
+	private int dId;
+	public int getpId() {
+		return pId;
+	}
+	public void setpId(int pId) {
+		this.pId = pId;
+	}
+	public String getpName() {
+		return pName;
+	}
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+	public int getdId() {
+		return dId;
+	}
+	public void setdId(int dId) {
+		this.dId = dId;
+	}
+}
